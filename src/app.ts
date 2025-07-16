cbf391b9e177f961130af63fd71d6fc2f8038e47
Hong Hai Nguyen <honghainguyenlqd@gmail.com>
2025-07-16 15:10:23 +0700

    .

--- src/app.ts
+++ src/app.ts
@@ -420,7 +420,7 @@ const createXR = async () => {
 
     const loadModel = async () => {
         // const modelUrl = "https://xensear-arworld.s3.ap-southeast-5.amazonaws.com/ar-world/AssetBundles/BabylonModelTest/";
-        const modelUrl = "https://www.dropbox.com/s/h0e15ypqmcm8f63gq3tzl/RingRing.glb";
+        const modelUrl = "https://dl.dropbox.com/scl/fi/h0e15ypqmcm8f63gq3tzl/RingRing.glb?rlkey=1jnot0nw8rl22voz55fv4542l&st=0rihhho0";
         
         const fileName = "RingRing.glb";
         const result = await SceneLoader.ImportMeshAsync("", modelUrl, "", scene);
